d83686e145fbb626f0a533482b1f2d57f7b703fb
Valerie Rinder <valerie.rinder@umontana.edu>
2018-04-23 08:00:36 -0600

    HW-9

---
+++ HW-9/readme.md
@@ -0,0 +1,4 @@
+This week was interesting. I'm glad I got to see The Coding Train videos. I decided to learn all about p5js.
+I also followed his instructions to get my own local server, though it took a lot of troubleshooting to finally get it up and running. I was just using jsfiddle before, and it didn't help much.
+I tried to do a more complex and unique site using NASA's api, but I ran into a lot of problems and confusion. I ended up making some adjustments to what I was working on
+with the coding train videos adding a few other p5js features and the cloud data from the api.
---
+++ HW-9/sketch.js
@@ -0,0 +1,50 @@
+
+/*variables*/
+var weather
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&APPID=e0bcdc8cb9c45d251e57c9725cb248a1';
+var units = '&units=metric';
+var input;
+var createCanvas;
+var h1;
+
+
+/*functions*/
+function setup() {
+canvas =  createCanvas(600, 400);
+canvas.parent('sketch-holder');
+h1 = createElement ('h1', 'Choose a city to see a thing!')
+var button1 = select('#submit');
+  button1.mousePressed(weatherAsk);
+  input = select('#city');
+}
+
+function mousePressed() {
+  h1.html("Clouds, Humidity, Temperature")
+createP("Try Another City!")
+}
+
+function weatherAsk() {
+  var url = api + input.value() + apiKey + units;
+  loadJSON(url, gotData);
+}
+
+function gotData(data) {
+  weather = data;
+}
+
+function draw() {
+  background(89, 113, 137);
+  if (weather) {
+    var temp = weather.main.temp;
+    var humidity = weather.main.humidity;
+    var all = weather.clouds.all;
+    var deg = weather.clouds.deg;
+
+    ellipse(500, 200, temp, temp);
+    ellipse(300, 200, humidity, humidity);
+    ellipse(100, 200, all, all);
+      text(input.value(), 10, 20);
+
+  }
+}
