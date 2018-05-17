78d8b5b8530563bf272ebb64f9942ec0b311d224
Valerie Rinder <valerie.rinder@umontana.edu>
2018-05-17 17:07:39 -0600

    move final to valerie

--- HW-Final/assets/js/min/main-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(window).scroll(function(){$(this).scrollTop()>480?($(".header").addClass("header--sticky"),$("body").addClass("is-scrolling")):($(".header").removeClass("header--sticky"),$("body").removeClass("is-scrolling"))})});
